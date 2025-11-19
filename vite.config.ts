b77e70063533f166118ba6a6a64e79edec2cac26
melia7676 <ameliakinsey5@gmail.com>
2025-11-19 13:16:05 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  plugins: [react()],
+  base: "/",
   server: {
     host: "::",
     port: 8080,
