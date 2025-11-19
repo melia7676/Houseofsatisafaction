b1723c925220b1e96c8c6a05e1d53a86ddc4324b
melia7676 <ameliakinsey5@gmail.com>
2025-11-19 13:41:57 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,8 @@
 // }));
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { componentTagger } from "lovable-tagger";
+
 
 export default defineConfig({
   plugins: [react()],
